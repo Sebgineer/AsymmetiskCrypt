d86595cf70b58ac69ca9eb84c90e8d3fed3519a4
Sebgineer <sebastian-karlsen@hotmail.com>
2022-06-24 10:27:16 +0200

    Add project files.

---
+++ AsymmetiskCrypt/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AsymmetiskCrypt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Encoding.UTF8.GetString(test(Encoding.UTF8.GetBytes("Kagemand"))));
+            RSA rsa = new RSA();
+
+            byte[] kage = Encoding.UTF8.GetBytes("Kagemand");
+            byte[] crypted = rsa.Encryption(kage, rsa.myRSA.ExportParameters(false), true);
+            
+            
+            byte[] decrypted = rsa.Decrypt(crypted, rsa.myRSA.ExportParameters(false), true);
+
+            Console.WriteLine(Encoding.UTF8.GetString(decrypted));
+            
+        }
+
+        //static byte[] test(byte[] text)
+        //{
+        //    byte[] RSAcipherText;
+        //    byte[] PlainText;
+        //    RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048);
+        //    AesManaged aesManaged = new AesManaged();
+        //    aesManaged.GenerateKey();
+        //    RSAcipherText = rsa.Encrypt(text, true);
+        //    return rsa.Decrypt(RSAcipherText, true);
+        //}
+    }
+}
---
+++ AsymmetiskCrypt/RSA.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace AsymmetiskCrypt
+{
+    internal class RSA
+    {
+        private RSACryptoServiceProvider rsa;
+
+        public RSACryptoServiceProvider myRSA { get { return this.rsa; } }
+
+        public RSA()
+        {
+            this.rsa = new RSACryptoServiceProvider(2048);
+        }
+
+        public byte[] Encryption(byte[] message, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            byte[] encryptedData;
+            rsa.ImportParameters(RSAKey);
+            encryptedData = rsa.Encrypt(message, DoOAEPPadding);
+            return encryptedData;
+        }
+
+        public byte[] Decrypt(byte[] data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            byte[] decryptedData;
+            rsa.ImportParameters(RSAKey);
+            decryptedData = this.rsa.Decrypt(data, DoOAEPPadding);
+            return decryptedData;
+        }
+
+        public void importParameter(RSAParameters RSAKey)
+        {
+            rsa.ImportParameters(RSAKey);
+
+        }
+    }
+}
